fe106a60b59430ae212009af939b78fff23f4ad1
Andreas Waschinski <5221082+waschinski@users.noreply.github.com>
2021-06-11 09:15:26 +0200

    Sorting photos by modified_time

--- _plugins/photo_filter.rb
+++ _plugins/photo_filter.rb
@@ -4,7 +4,8 @@ module Jekyll
   module PhotoFilter
     def photo_filter(files)
       photos = files.select {|photo| photo.relative_path.include?("original") }
-      photos.sort_by do |photo|
+      sorted = photos.sort_by { |photo| photo.modified_time }
+      sorted.each do |photo|
         EXIFR::JPEG.new(photo.path).date_time.to_s
       end.reverse
     end
