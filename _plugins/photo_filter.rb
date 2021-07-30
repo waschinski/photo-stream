f798461608305ee49bc3adebf4327f6fc92c3e19
Dan Bush <dan@knows.email>
2021-07-30 14:32:21 -0500

    reverted line 7

--- _plugins/photo_filter.rb
+++ _plugins/photo_filter.rb
@@ -4,7 +4,7 @@ module Jekyll
   module PhotoFilter
     def photo_filter(files)
       photos = files.select {|photo| photo.relative_path.include?("original") }
-      sorted = photos.sort_by { |photo| EXIFR::JPEG.new(photo.path).date_time_original.to_s }
+      sorted = photos.sort_by { |photo| photo.modified_time }
       sorted.each do |photo|
         EXIFR::JPEG.new(photo.path).date_time_original.to_s
       end.reverse
